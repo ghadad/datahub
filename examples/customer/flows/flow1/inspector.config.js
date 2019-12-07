d27e1527e5adb19a35d8f7318deeca243978b02d
ghadad <ghadad@gmail.com>
2019-12-07 22:11:10 +0200

    wip

--- examples/customer/flows/flow1/inspector.config.js
+++ examples/customer/flows/flow1/inspector.config.js
@@ -1,8 +1,1 @@
-module.exports = function(data) { 
-    // get last active record
-    // get current record 
-    // compare by key def 
-    // finilze active data
-    // finilize dhanges delta 
-    // update target entities and databases 
-}
+module.exports ={}
