3abe9b0214a276c6bbe4c65ea74916ca39cbb1b7
Golan Hadad <ghadad@gmail.com>
2019-12-15 23:06:34 +0200

    wip

--- examples/customer/flows/flow2/mapping.config.js
+++ examples/customer/flows/flow2/mapping.config.js
@@ -1,4 +1,42 @@
-module.exports =  [
-  { fieldPosition:2 , goTo:"companyName" },
+module.exports = [{
+    hash: true,
+    key: "name",
+    goTo: "firstName",
+    valid: __app.validator.maxlen(10)
+  },
+  {
+    hash: true,
+    key: "name",
+    handler: (r, n, v) => v + ":" + v,
+    goTo: "lastName"
+  },
+  {
+    hash: false,
+    const: 12345,
+    goTo: "companyId",
+    valid: __app.validator.in(12345, 43221),
+  },
+  {
+    hash: false,
+    query: "select 1 as val  from dual",
+    //poolAlias :"any",
+    goTo: "queryResult",
+    valid: __app.validator.lt(4),
+  },
+  {
+    hash: false,
+    package: "mypackage.run()",
+    handler: (r, n, v) => v.length,
+    //poolAlias :"any",
+    goTo: "queryResult",
+    valid: __app.validator.lt(4),
+  },
+  {
+    const: "4",
+    //poolAlias :"any",
+    goTo: "queryResult",
+    transform: __app.lodash.toNumber(4),
+    valid: __app.validator.between(3, 8),
+    drop: [__app.validator.notnull(), __app.validator.between(10, 13)]
+  }
 ]
-
