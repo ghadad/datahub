1b8dd39d29f1a8c42f5f1f865720177eae120b9d
Golan Hadad <ghadad@gmail.com>
2019-12-17 22:54:39 +0200

    wip

--- examples/customer/flows/flow1/discover.handler.js
+++ examples/customer/flows/flow1/discover.handler.js
@@ -1,9 +1,9 @@
-module.exports=  async function(data) { 
+module.exports = async function (data) {
    //all good things are exposed 
    // __app.dal
    // __app.logger
    // __app.lodash
    // etc ..
-   __app.logger.info("in inspector handler:",data);
+   __app.logger.info("in discovery  handler:", data);
    return data;
 }
