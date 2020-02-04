86f1ecd46a98d6f72e7cc1da8ae41a2784d5d0cb
golanh <golanh@localhost.localdomain>
2020-02-04 17:45:00 +0200

    wip

--- templates/project/flows/flow1/collector.handler.js
+++ templates/project/flows/flow1/collector.handler.js
@@ -1,9 +1,9 @@
-module.exports=  async function(data) { 
+module.exports = async function (newData, oldData, raw) {
    //all good things are exposed 
    // __app.dal
    // __app.logger
    // __app.lodash
    // etc ..
-   __app.logger.info("in collector handler:",data);
+   __app.logger.info("in collector handler:", data);
    return data;
 }
