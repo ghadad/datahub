66f8fbfa30d6fc312c53d33b8339f5b328a43bd8
golanh <golanh@localhost.localdomain>
2020-05-02 01:47:41 +0300

    wip

--- bin/install.js
+++ bin/install.js
@@ -24,26 +24,30 @@ main.init(argv).then(async function () {
   await __app.couchDb.createDbIfNotExists("datasets");
   await __app.couchDb.createDbIfNotExists("sqls");
   await __app.couchDb.createDbIfNotExists("config");
-  await __app.couchDb.createDbIfNotExists("errors");
+  await __app.couchDb.createDbIfNotExists("hub_errors");
   await __app.couchDb.createDbIfNotExists("jobs");
   await __app.couchDb.createDbIfNotExists("interfaces");
+  await __app.couchDb.createDbIfNotExists("interface_errors");
+
 
   //create config doc per env
 
 
-  await __app.couchDb.create("config", {
+  await __app.couchDb.createDbIfNotExists("config", {
     _id: "dev"
   });
-  await __app.couchDb.create("config", {
+  await __app.couchDb.createDbIfNotExists("config", {
     _id: "qa"
   });
-  await __app.couchDb.create("config", {
+  await __app.couchDb.createDbIfNotExists("config", {
     _id: "prod"
   });
 
+  await __app.couchDb.createIndex("hub_errors", "hub_error_idx", ["entityId"]);
+  await __app.couchDb.createIndex("interface_errors", "interface_error_idx", ["entityId", "runId"]);
 
 
 }).catch(function (err) {
-  __app.logger.error("flow exec failed:", err.stack);
+  __app.logger.error("install failed:", err.stack);
   process.exit(-1)
 });
